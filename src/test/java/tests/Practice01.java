6b302800001cb49dfb1b296350ff64fb44580220
Nurullah Taşkan <nttaskan@gmail.com>
2023-02-24 10:34:59 +0300

    23/02/2023

---
+++ src/test/java/tests/Practice01.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.DhtmlPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class Practice01 {
+    DhtmlPage dhtmlPage;
+    @Test
+    public void testName() {
+        dhtmlPage=new DhtmlPage();
+        //http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html adresine gidiniz
+        Driver.getDriver().get(ConfigReader.getProperty("dhtmlUrl"));
+        //Tüm ülkeleri yazdırınız
+
+        for (WebElement w: dhtmlPage.ulkeler){
+            System.out.println("Ulkeler = " + w.getText());
+        }
+
+        //Ülkeler ile şehirleri eşleştiriniz
+        Actions actions=new Actions(Driver.getDriver());
+        actions.dragAndDrop(dhtmlPage.Seoul,dhtmlPage.Korea).
+                dragAndDrop(dhtmlPage.oslo,dhtmlPage.Norway).
+                dragAndDrop(dhtmlPage.Stockholm,dhtmlPage.Sweden).
+                dragAndDrop(dhtmlPage.Washington,dhtmlPage.abd).
+                dragAndDrop(dhtmlPage.Rome,dhtmlPage.Italy).
+                dragAndDrop(dhtmlPage.Madrid,dhtmlPage.Spain).
+                dragAndDrop(dhtmlPage.Copenhagen,dhtmlPage.Denmark).perform();
+
+    }
+
+}
